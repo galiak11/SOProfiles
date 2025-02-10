004ef99c7b1a65793dc85d10ff01fdcef7838da3
Galia Kaufman <galiak@google.com>
2025-02-09 22:42:24 -0500

    New SOProfiles XCode project

---
+++ SOProfilesTests/SOProfilesTests.swift
@@ -0,0 +1,17 @@
+//
+//  SOProfilesTests.swift
+//  SOProfilesTests
+//
+//  Created by Galia Kaufman on 2/9/25.
+//
+
+import Testing
+@testable import SOProfiles
+
+struct SOProfilesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
